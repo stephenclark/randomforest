733e738d3d5accc9e83aa34ba4fcc1b3d4ea72cc
stephenclark <stephen.k.clark@gmail.com>
2017-05-09 20:46:53 +1000

    clones from git rather than from s3

--- awsscript.sh
+++ awsscript.sh
@@ -1,57 +1,24 @@
 #!/bin/bash
 # get the things we need 
 yum update -y
-yum -y install  mysql mysql-server
+yum -y install  mysql mysql-server git
 pip install Flask
 
 # start the required services 
 service mysqld start
 chkconfig mysqld on
 
+git clone https://github.com/stephenclark/randomforest.git 
 
-# go out to github and get the site - for now just use s3
-
-mkdir templates
-
-aws s3 cp s3://mq-random-forest/index.html /var/www/html
-aws s3 cp s3://mq-random-forest/table1.csv /home/ec2-user/table1.csv
-aws s3 cp s3://mq-random-forest/table2.csv /home/ec2-user/table2.csv
-aws s3 cp s3://mq-random-forest/create_db.SQL /home/ec2-user/create_db.SQL
-aws s3 cp s3://mq-random-forest/templates/form.html /home/ec2-user/templates/form.html
+#aws s3 cp s3://mq-random-forest/table1.csv /home/ec2-user/table1.csv
+#aws s3 cp s3://mq-random-forest/table2.csv /home/ec2-user/table2.csv
+#aws s3 cp s3://mq-random-forest/create_db.SQL /home/ec2-user/create_db.SQL
+#aws s3 cp s3://mq-random-forest/templates/form.html /home/ec2-user/templates/form.html
 
 # setup the database
 mysqladmin -u root password veryrandompassword
 mysql  -u root -pveryrandompassword < create_db.SQL
 
 
-# set up flask and start the website.
-pip install Flask
-
-
-
-
-# start flask 
-echo "from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'flask is working !!!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug = True)" > app.py
-python app.py
-
-
-
-#mysqladmin -u root -pveryrandompassword create mq_random_forest
-# mysql -h mq-random-forest.c7vffwba7rx2.ap-southeast-2.rds.amazonaws.com -P 3306 -u randomForestUser -pveryrandompassword < create_db.SQL
-# sudo rm create_db.SQL  table1.csv  table2.csv
-#sudo yum install httpd -y
-#sudo service httpd start
-#sudo chkconfig httpd on
-#sudo cd /var/www/html
-# assuming region is ap-southeast-2a
-#sudo aws s3 cp s3://mq-random-forest/index.html /var/www/html
-
+#start the website. Should be listening on port 80
+python randomforest/app.py
